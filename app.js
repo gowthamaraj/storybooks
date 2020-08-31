d57f795772acebfb2e3f89b47e941cb6b194e781
gowthamaraj <gowthamaraj360@gmail.com>
2020-08-31 15:51:41 +0530

    basic file structure done

--- app.js
+++ app.js
@@ -9,13 +9,26 @@ const passport = require('passport')
 const session = require('express-session')
 const { extname } = require('path')
 const MongoStore = require('connect-mongo')(session)
+const connectDB = require('./config/db')
 
+//Load config
+dotenv.config({path:'./config/.env'})
+
+//require passport
+require('./config/passport')
+
+//connectDB()
 
 const app = express()
 
 //parse the body
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
+
+// Logging
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'))
+  }
 
 //hbs
 app.engine('.hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}))
@@ -24,5 +37,12 @@ app.set('view engine', '.hbs')
 // Static folder
 app.use(express.static(path.join(__dirname, 'public')))
 
+// Routes
+app.use('/', require('./routes/index'))
+app.use('/auth', require('./routes/auth'))
+app.use('/stories', require('./routes/stories'))
+
 const PORT = process.env.PORT||3000
 app.listen(PORT,console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
+
+
